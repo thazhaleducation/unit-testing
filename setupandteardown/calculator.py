b0aa523c0a8ad48cfe5c35ffdea4f4021bb304c9
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-10-30 13:34:02 +0530

    Add exception assertion

--- setupandteardown/calculator.py
+++ setupandteardown/calculator.py
@@ -1,3 +1,6 @@
 class Calculator:
   def add(self, op1, op2):
     return op1 + op2
+
+  def divide(self, op1, op2):
+    return op1 / op2
--- setupandteardown/test_calculator.py
+++ setupandteardown/test_calculator.py
@@ -5,24 +5,34 @@ from setupandteardown.calculator  import Calculator
 
 class CalculatorTestCase(unittest.TestCase):
 
+  #Test Fixture
+  def setUp(self) -> None:
+    self.calc = Calculator()
+    return super().setUp()
+
   def test_add_should_return_addition_of_two_positive_numbers(self):
-    calc = Calculator()
-    actual_result = calc.add(1, 2)
+    actual_result = self.calc.add(1, 2)
     expected_result = 3
-    assert actual_result == expected_result
+    self.assertEqual(actual_result, expected_result)
   
   def test_add_should_return_addition_of_two_negative_numbers(self):
-    calc = Calculator()
-    actual_result = calc.add(-1, -3)
+    actual_result = self.calc.add(-1, -3)
     expected_result = -4
-    assert actual_result == expected_result
+    self.assertEqual(actual_result, expected_result)
   
   def test_return_addition_of_two_numbers_when_one_of_the_operand_is_negative(self):
-    calc = Calculator()
-    actual_result = calc.add(-1, 2)
+    actual_result = self.calc.add(-1, 2)
     expected_result = 1
-    assert actual_result == expected_result
-
+    self.assertEqual(actual_result, expected_result)
+  
+  def test_divide_should_return_result_of_division_of_two_numbers(self):
+    actual_result = self.calc.divide(4, 2)
+    expected_result = 2
+    self.assertEqual(actual_result, expected_result)
+  
+  def test_divide_should_raise_exception_when_divided_by_zero(self):
+    with self.assertRaises(ZeroDivisionError):
+      self.calc.divide(4, 0)
 
 if __name__ == '__main__':
     unittest.main()
