e13caf8b8bec50320f0e591d71d4a89b403c895f
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2022-11-03 22:09:05 +0530

    Update phone example

--- testingdeps/test_phone.py
+++ testingdeps/test_phone.py
@@ -4,13 +4,16 @@ from testingdeps.phone import Phone
 from testingdeps.battery import Battery
 
 class PhoneTestCase(unittest.TestCase):
+  def setUp(self):
+    self.phone = Phone( )
+    return super().setUp()
+
   def test_should_return_true_for_enable_power_save_mode_when_battery_low(self):
     phone_with_low_battery = Phone(Battery(10))
     self.assertTrue(phone_with_low_battery.enable_power_save_mode())
   
   def test_should_return_false_for_enable_power_save_mode_when_battery_is_not_low(self):
-    phone_with_full_battery = Phone(Battery())
-    self.assertFalse(phone_with_full_battery.enable_power_save_mode())
+    self.assertFalse(self.phone.enable_power_save_mode())
   
 if __name__ == '__main__':
     unittest.main()
